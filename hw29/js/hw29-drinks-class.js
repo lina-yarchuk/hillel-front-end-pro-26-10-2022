5e8542ec9b021e494f172f42e39c0a22094a9b94
lina-yarchuk <lina.yarchuk@gmail.com>
2023-02-08 20:31:03 +0200

    hw-29 Drinks

--- hw29/js/hw29-drinks-class.js
+++ hw29/js/hw29-drinks-class.js
@@ -1,19 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-    function uniqueList(listElement) {
-        const listItems = listElement.querySelectorAll('li');
+// duplication code with class (just for training but review please)
+
+class List {
+    #unique = null;
+
+    constructor(selector) {
+        this.element = document.querySelector(selector);
+        this.#unique = this.#uniqueList();
+
+        this.render();
+    }
+
+    #uniqueList() {
+        const listItems = this.element.querySelectorAll('li');
         const itemsArray = Array.from(listItems, elems => elems.textContent);
 
-        // return itemsArray.filter((text, index) => itemsArray.indexOf(text) === index).sort();
-        // return itemsArray.reduce((acc, text) => !acc.includes(text) ? [...acc, text] : acc, []).sort();
         return [...new Set(itemsArray)].sort();
     }
 
-    function renderUniqueList(selector) {
-        const listElement = document.querySelector(selector);
-        const unique = uniqueList(listElement);
-
-        listElement.innerHTML = unique.reduce((acc, text) => `${acc}<li>${text}</li>`, '');
+    render() {
+        this.element.innerHTML = this.#unique.reduce((acc, text) => `${acc}<li>${text}</li>`, '');
     }
+}
 
-    renderUniqueList('#drinks-list');
+document.addEventListener('DOMContentLoaded', () => {
+    new List('#drinks-list');
 });
